64b9af9130a4ce2e741d657f50aed30fb3747abf
nicholaspang <11019412@qq.com>
2018-07-06 17:11:19 +0800

    init

---
+++ management-web/src/main/java/com/fooee/winqing/management/web/controller/system/MainController.java
@@ -0,0 +1,14 @@
+package com.fooee.winqing.management.web.controller.system;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * description
+ *
+ * @author pangzhenhua
+ * @date 2018/7/6
+ */
+@Controller
+public class MainController {
+
+}
